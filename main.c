6623f97d3d78401748adacca9812619f9d3cca97
programadorsg <48134200+programadorsg@users.noreply.github.com>
2019-03-01 14:58:45 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+
+    printf("Informe um numero inteiro: ");
+    scanf("%d", &num);
+
+    printf("Base hexadecimal: %X", num);
+
+    return 0;
+}
